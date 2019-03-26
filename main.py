b5a76306e79a96c85a473c5292464c67fced0e25
Marth8 <nicolai.froehlig@gmail.com>
2019-03-26 17:08:38 +0100

    reworked neuronal networks, renamed gulaschSamples to testSamples

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ import os ;
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import matplotlib.pyplot as plt;
 
-mnistPath = "./gulasch8LanesWithCar.pkl.gz"
+bigSamplesPath = "./bigSamples.pkl.gz";
+testDataPath = "./testSamples.pkl.gz";
 
-
-with gzip.open(mnistPath, 'rb') as f:
+with gzip.open(bigSamplesPath, 'rb') as f:
   #python3
   #tmp = pickle.load(f, encoding='bytes')
   # python2
   tmp = pickle.load(f) ;
 
-  # box mit 3750 x 5
   trainlBox = tmp["trainlBox"]
   trainlLane = tmp["trainlLane"]
   traind = tmp["traind"]
@@ -40,19 +39,55 @@ with gzip.open(mnistPath, 'rb') as f:
   print(trainlBox.max(axis = 0).min());
   print(trainlBox.max(axis = 0).max());
 
+with gzip.open(testDataPath, 'rb') as f:
+  #python3
+  #tmp = pickle.load(f, encoding='bytes')
+  # python2
+  tmp = pickle.load(f) ;
+
+  testlBox = tmp["trainlBox"]
+  testlLane = tmp["trainlLane"]
+  testd = tmp["traind"]
+
+  # check input shapes
+  print(testlBox.shape)
+  print(testlLane.shape)
+  print(testd.shape)
+
+  # check box values
+  print(testlBox[0]);
+
+  #check box values for nan vakzes
+  print(np.any(np.isnan(testlBox)));
+
+  # check min and max of box values
+  print(testlBox.min(axis = 0).min());
+  print(testlBox.min(axis = 0).max());
+  print(testlBox.max(axis = 0).min());
+  print(testlBox.max(axis = 0).max());
+
 # analyse data
-
 # check for block sorting
-print("are them block sorted?", trainlLane.argmax(axis=1))
+print("are the bigSamples block sorted?", trainlLane.argmax(axis=1))
+print("are the testSamples block sorted?", testlLane.argmax(axis=1));
 
-# re-shuffle
+# re-shuffle bigSamples
 nrTrainSamples = traind.shape[0]
 indices = np.arange(0,nrTrainSamples) # alle indices der trainsamples holen
 np.random.shuffle(indices) # alles indices shufflen, muss nicht zugewiesen werden, da implace "ersetzend"
 traind = traind[indices];
 trainlBox = trainlBox[indices] # fancyIndexing: Warum nicht beide einzeln? Da sonst der index ein anderern waere bei data und labels
 trainlLane = trainlLane[indices]
-print ("shuffledIndices", trainlLane.argmax(axis=1))
+print ("shuffledIndices bigSamples", trainlLane.argmax(axis=1))
+
+# re-shuffle testSamples
+nrTrainSamples = testd.shape[0]
+indices = np.arange(0, nrTrainSamples) # alle indices der trainsamples holen
+np.random.shuffle(indices) # alles indices shufflen, muss nicht zugewiesen werden, da implace "ersetzend"
+testd = testd[indices];
+testlBox = testlBox[indices];
+testlLane = testlLane[indices];
+print ("shuffledIndices testSamples", trainlLane.argmax(axis=1))
 
 # check min and max of pictures
 dmin = traind.min(axis = 0) # hole die kleinste Zeile! wegen traind axis 0
@@ -66,36 +101,40 @@ print("max von max: ", dmax.max()) # max zischen 1 und 10000 --> hist plotten
 print(dmax.argmax()) # finde index an welcher position 10000
 print(dmin.argmax()) #selbe!
 
-# check number of samples per class
+# check number of samples per class ong bigSamplesLabels
 samplesPerClass = trainlLane.sum(axis=0);
-print("samplesPerClass", samplesPerClass);
+print("samplesPerClass bigSamples", samplesPerClass);
+
+# check number of samples per class on testlabels
+samplesPerClass = testlLane.sum(axis=0);
+print("samplesPerClass testSamples", samplesPerClass);
 
 with tf.Session() as sess:
-  ## input layer: Nx100x120x1
-  dataPlaceholder = tf.placeholder(tf.float32, shape=[None, 100, 120, 1]);
+  ## input layer: Nx108x192x1
+  dataPlaceholder = tf.placeholder(tf.float32, shape=[None, 108, 192, 1]);
   labelPlaceholder = tf.placeholder(tf.float32,[None, 8]) ;
   boxPlaceholder = tf.placeholder(tf.float32, shape=[None, 8]);
 
-  ## Hidden Layer 1: 96x116x6 => 48x58x6
+  ## Hidden Layer 1: 104x188x6 => 52x94x6
   # Convolution Layer with 32 fiters and a kernel size of 5
   conv1 = tf.nn.relu(tf.layers.conv2d(dataPlaceholder,6, 5,name="H1")) ;
   print (conv1) ;
   a1 = tf.layers.max_pooling2d(conv1, 2, 2) ;
   print (a1) ;
 
-  ## Hidden Layer 2: 44x54x16 => 22x27x16
+  ## Hidden Layer 2: 48x90x16 => 24x45x16
   conv2 = tf.nn.relu(tf.layers.conv2d(a1, 16, 5,name="H2")) ;
   a2 = tf.layers.max_pooling2d(conv2, 2, 2) ;
   print (a2) ;
-  a2flat = tf.reshape(a2, (-1, 22 * 27 * 16)) ;
+  a2flat = tf.reshape(a2, (-1, 24 * 45 * 16)) ;
 
-  # Den Vector der BoundingBox anhängen => 22x27x16 + 8
+  # Den Vector der BoundingBox anhängen => 24x45x16 + 8
   a2flat = tf.concat([a2flat, boxPlaceholder], axis=1);
 
   ## Hidden Layer 3
   Z3 = 120 ;
   # allocate variables
-  W3 = tf.Variable(np.random.uniform(-0.01,0.01, [22 * 27 * 16 + 8,Z3]),dtype=tf.float32, name ="W3") ; # adjust weights for a2flat!
+  W3 = tf.Variable(np.random.uniform(-0.01,0.01, [24 * 45 * 16 + 8,Z3]),dtype=tf.float32, name ="W3") ; # adjust weights for a2flat!
   b3 = tf.Variable(np.random.uniform(-0.01,0.01, [1,Z3]),dtype=tf.float32, name ="b3") ;
   # compute activations
   a3 = tf.nn.relu(tf.matmul(a2flat, W3) + b3) ;
@@ -113,7 +152,7 @@ with tf.Session() as sess:
 
   ## output layer
   # alloc variables
-  Z5 = 8 ; # muss nicht geaendert werden, da wir hier immer noch 10 labels haben
+  Z5 = 8 ;
   W5 = tf.Variable(np.random.uniform(-0.1,0.1, [Z4,Z5]),dtype=tf.float32, name ="W5") ;
   b5 = tf.Variable(np.random.uniform(-0.01,0.01, [1,Z5]),dtype=tf.float32, name ="b5") ;
   # compute activations
@@ -164,6 +203,5 @@ with tf.Session() as sess:
     # compute loss and accuracy, only every 50 iterations to save time
     if iteration%50==0:
       acc, lossVal= sess.run([nrCorrect, loss], feed_dict =  {dataPlaceholder: dataBatch, labelPlaceholder : labelBatch, boxPlaceholder: boxBatch }) ;
-      #testacc = sess.run(nrCorrect, feed_dict = {data_placeholder: testd, label_placeholder: testl})
-      #print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal, "testacc=",testacc) ;
-      print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal)
+      testacc = sess.run(nrCorrect, feed_dict = {dataPlaceholder: testd, labelPlaceholder: testlLane, boxPlaceholder: testlBox});
+      print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal, "testacc=",testacc) ;
--- mainMaskFull.py
+++ mainMaskFull.py
@@ -1,24 +1,19 @@
 # LeNet-5
-#import matplotlib as mp ;
-#mp.use("Qt4Agg") ;
 import gzip, pickle;
 import numpy as np;
-#import numpy.random as npr
 import tensorflow as tf;
 import os ;
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import matplotlib.pyplot as plt;
 
-mnistPath = "./gulasch.pkl.gz"
+bigSamplesPath = "./bigSamples.pkl.gz";
+testDataPath = "./testSamples.pkl.gz";
 
-
-with gzip.open(mnistPath, 'rb') as f:
+with gzip.open(bigSamplesPath, 'rb') as f:
   #python3
   #tmp = pickle.load(f, encoding='bytes')
   # python2
   tmp = pickle.load(f) ;
 
-  # box mit 3750 x 5
   trainlLane = tmp["trainlLane"]
   traind = tmp["trainlPictureBB_full"]
 
@@ -26,18 +21,40 @@ with gzip.open(mnistPath, 'rb') as f:
   print(trainlLane.shape)
   print(traind.shape)
 
+with gzip.open(testDataPath, 'rb') as f:
+  #python3
+  #tmp = pickle.load(f, encoding='bytes')
+  # python2
+  tmp = pickle.load(f) ;
+
+  testlLane = tmp["trainlLane"]
+  testd = tmp["trainlPictureBB_full"]
+
+  # check input shapes
+  print(testlLane.shape)
+  print(testd.shape)
+
+
 # analyse data
-
 # check for block sorting
-print("are them block sorted?", trainlLane.argmax(axis=1))
+print("are the bigSamples block sorted?", trainlLane.argmax(axis=1))
+print("are the testSamples block sorted?", testlLane.argmax(axis=1));
 
-# re-shuffle
+# re-shuffle bigSamples
 nrTrainSamples = traind.shape[0]
 indices = np.arange(0,nrTrainSamples) # alle indices der trainsamples holen
 np.random.shuffle(indices) # alles indices shufflen, muss nicht zugewiesen werden, da implace "ersetzend"
 traind = traind[indices];
 trainlLane = trainlLane[indices]
-print ("shuffledIndices", trainlLane.argmax(axis=1))
+print ("shuffledIndices bigSamples", trainlLane.argmax(axis=1))
+
+# re-shuffle testSamples
+nrTrainSamples = testd.shape[0]
+indices = np.arange(0, nrTrainSamples) # alle indices der trainsamples holen
+np.random.shuffle(indices) # alles indices shufflen, muss nicht zugewiesen werden, da implace "ersetzend"
+testd = testd[indices];
+testlLane = testlLane[indices];
+print ("shuffledIndices testSamples", trainlLane.argmax(axis=1))
 
 # check min and max of pictures
 dmin = traind.min(axis = 0) # hole die kleinste Zeile! wegen traind axis 0
@@ -51,32 +68,37 @@ print("max von max: ", dmax.max()) # max zischen 1 und 10000 --> hist plotten
 print(dmax.argmax()) # finde index an welcher position 10000
 print(dmin.argmax()) #selbe!
 
-# check number of samples per class
+# check number of samples per class ong bigSamplesLabels
 samplesPerClass = trainlLane.sum(axis=0);
-print("samplesPerClass", samplesPerClass);
+print("samplesPerClass bigSamples", samplesPerClass);
+
+# check number of samples per class on testlabels
+samplesPerClass = testlLane.sum(axis=0);
+print("samplesPerClass testSamples", samplesPerClass);
+
 
 with tf.Session() as sess:
-  ## input layer: Nx100x240x1
-  dataPlaceholder = tf.placeholder(tf.float32, shape=[None, 100, 240, 1]);
+  ## input layer: Nx108x384x1
+  dataPlaceholder = tf.placeholder(tf.float32, shape=[None, 108, 384, 1]);
   labelPlaceholder = tf.placeholder(tf.float32,[None, 8]) ;
 
-  ## Hidden Layer 1: 96x236x6 => 48x118x6
+  ## Hidden Layer 1: 104x380x6 => 52x190x6
   # Convolution Layer with 32 fiters and a kernel size of 5
   conv1 = tf.nn.relu(tf.layers.conv2d(dataPlaceholder,6, 5,name="H1")) ;
   print (conv1) ;
   a1 = tf.layers.max_pooling2d(conv1, 2, 2) ;
   print (a1) ;
 
-  ## Hidden Layer 2: 44x114x16 => 22x57x16
+  ## Hidden Layer 2: 48x186x16 => 24x93x16
   conv2 = tf.nn.relu(tf.layers.conv2d(a1, 16, 5,name="H2")) ;
   a2 = tf.layers.max_pooling2d(conv2, 2, 2) ;
   print (a2) ;
-  a2flat = tf.reshape(a2, (-1, 22 * 57 * 16)) ;
+  a2flat = tf.reshape(a2, (-1, 24 * 93 * 16)) ;
 
   ## Hidden Layer 3
   Z3 = 120 ;
   # allocate variables
-  W3 = tf.Variable(np.random.uniform(-0.01,0.01, [22 * 57 * 16,Z3]),dtype=tf.float32, name ="W3") ; # adjust weights for a2flat!
+  W3 = tf.Variable(np.random.uniform(-0.01,0.01, [24 * 93 * 16,Z3]),dtype=tf.float32, name ="W3") ; # adjust weights for a2flat!
   b3 = tf.Variable(np.random.uniform(-0.01,0.01, [1,Z3]),dtype=tf.float32, name ="b3") ;
   # compute activations
   a3 = tf.nn.relu(tf.matmul(a2flat, W3) + b3) ;
@@ -144,6 +166,5 @@ with tf.Session() as sess:
     # compute loss and accuracy, only every 50 iterations to save time
     if iteration%50==0:
       acc, lossVal= sess.run([nrCorrect, loss], feed_dict =  {dataPlaceholder: dataBatch, labelPlaceholder : labelBatch}) ;
-      #testacc = sess.run(nrCorrect, feed_dict = {data_placeholder: testd, label_placeholder: testl})
-      #print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal, "testacc=",testacc) ;
-      print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal)
+      testacc = sess.run(nrCorrect, feed_dict = {dataPlaceholder: testd, labelPlaceholder: testlLane})
+      print ("epoch=", nrEpochs, "iteration=", iteration, ", acc=", float(acc), "loss=", lossVal, "testacc=",testacc) ;
